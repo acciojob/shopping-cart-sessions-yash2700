dbcd73e953aaeed1f5c3fb848d6abfcf916e77a7
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 20:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,30 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
-
- // DOM elements
+// DOM elements
 const productList = document.getElementById("product-list");
-const cartList = document.getElementById("cart-list");
-const clearCartBtn = document.getElementById("clear-cart-btn");
 
-// Get cart data from session storage or initialize it
-let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+// Render product list
+function renderProducts() {
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    productList.appendChild(li);
+  });
+}
+
+// Render cart list
+function renderCart() {}
+
+// Add item to cart
+function addToCart(productId) {}
+
+// Remove item from cart
+function removeFromCart(productId) {}
+
+// Clear cart
+function clearCart() {}
+
+// Initial render
+renderProducts();
+renderCart();
